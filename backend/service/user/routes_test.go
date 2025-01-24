d5bf65555297d4736ed83b576254f63abdea5354
Timamora <timroma6@gmail.com>
2025-01-24 16:25:08 -0500

    added successful checks in the test section

--- backend/service/user/routes_test.go
+++ backend/service/user/routes_test.go
@@ -52,14 +52,47 @@ func TestUserServiceHandlers(t *testing.T) {
 		}
 	})
 
-	t.Run("Should correctly register the user", func(t *testing.T) {})
+	t.Run("Should correctly register the user", func(t *testing.T) {
+		/*
+			Test with a valid payload. Should successfully create the user
+		*/
+
+		payload := types.RegisterUserPayload{
+			FirstName: "user",
+			LastName:  "lastName",
+			Password:  "invalid",
+			Email:     "sad@gmail.com",
+		}
+
+		//Youve got to marshal your struct into a json
+		marshalled, _ := json.Marshal(payload)
+
+		//Also need to transform it into a new byte buffer
+		req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
+
+		if err != nil {
+			//Automatically fails a test
+			t.Fatal(err)
+		}
+
+		//Actually make the request to our handler server
+		requestAgainstHandler := httptest.NewRecorder()
+		router := mux.NewRouter()
+		router.HandleFunc("/register", handler.handleRegister)
+		router.ServeHTTP(requestAgainstHandler, req)
+
+		//Our test shoulve failed so check that
+		if requestAgainstHandler.Code != http.StatusCreated {
+			t.Errorf("expected status code %d, got %d", http.StatusCreated, requestAgainstHandler.Code)
+		}
+	})
 }
 
 type mockUserStore struct{}
 
 // Methods needed for a user store need to be mocked
 func (s *mockUserStore) GetUserByEmail(email string) (*types.User, error) {
-	return nil, fmt.Errorf("email not found") //This needs to be mocked to isolate our handleRegister method
+	return nil, fmt.Errorf("email not found") //This needs to be mocked to ignore the case where user already exists
 }
 
 func (s *mockUserStore) GetUserByID(id int) (*types.User, error) {
