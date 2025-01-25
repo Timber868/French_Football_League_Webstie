98a081dc678626dac4d1a6ed1ff2172b8f43e044
Timamora <timroma6@gmail.com>
2025-01-25 00:42:30 -0500

    Finished up the registration and it works!

--- backend/types/types.go
+++ backend/types/types.go
@@ -7,7 +7,7 @@ type RegisterUserPayload struct {
 	FirstName string `json:"firstName" validate:"required"`
 	LastName  string `json:"lastName" validate:"required"`
 	Email     string `json:"email" validate:"required,email"`
-	Password  string `json:"password" validate:"required,min=3,max=130`
+	Password  string `json:"password" validate:"required,min=3,max=130"`
 }
 
 type User struct {
